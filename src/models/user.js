5d9de1dca88103e9932511a699fbd32abff8d7e1
josdank <josue.guerra@epn.edu.ec>
2024-12-01 20:52:52 -0500

    user_models

--- src/models/user.js
+++ src/models/user.js
@@ -4,17 +4,17 @@ import fetch from 'node-fetch';
 const userModel = {
     async registerUserModel(newUser) {
         try {
-            const url = 'https://usuariossoria.free.beeceptor.com/api/users';
+            const url = 'https://api-restful.free.beeceptor.com/api/users'; // Asegúrate de incluir la ruta correcta
             const peticion = await fetch(url, {
                 method: "POST",
                 body: JSON.stringify(newUser),
                 headers: { 'Content-Type': "application/json" }
             });
-
+    
             if (!peticion.ok) {
                 throw new Error(`Error en la petición: ${peticion.statusText}`);
             }
-
+    
             const data = await peticion.json();
             return data;
         } catch (error) {
@@ -22,10 +22,10 @@ const userModel = {
             throw new Error("Error al registrar el usuario");
         }
     },
-
+    
     async loginUserModel(username, password) {
         try {
-            const url = 'https://usuariossoria.free.beeceptor.com/api/users'; 
+            const url = 'https://api-restful.free.beeceptor.com/api/users'; // Usamos la ruta general de usuarios para login
             const peticion = await fetch(url, {
                 method: "POST",  
                 headers: { 'Content-Type': 'application/json' },
@@ -34,10 +34,12 @@ const userModel = {
     
             const user = await peticion.json();
     
+            // Verifica si la respuesta contiene un error o si no existe el usuario
             if (!user || user.error) {
                 return { error: "Username o password incorrectos" };
             }
     
+            // Compara la contraseña encriptada con la contraseña proporcionada
             const passwordMatch = await bcrypt.compare(password, user.password);
             if (!passwordMatch) {
                 return { error: "Username o password incorrectos" };
@@ -49,6 +51,8 @@ const userModel = {
             return { error: "Error en el login" };
         }
     }
+    
+    
 };
 
 export default userModel;
